0beaffba6e9c13f6ca22cede4dec928ec9618605
Jeff Hicks <jhicks@jdhitsolutions.com>
2023-06-27 19:10:40 -0400

    re-org

---
+++ functions/Get-LmhostsEntry.ps1
@@ -0,0 +1,95 @@
+Function Get-LmhostsEntry {
+[CmdletBinding(DefaultParameterSetName="Name")]
+Param(
+[Parameter(Position=0,Mandatory,ParameterSetName="Name")]
+[Alias("CN","Name")]
+[String]$Computername,
+[Parameter(Mandatory,ParameterSetName="IP")]
+[ValidatePattern("(\d{1,3}\.){3}\d{1,3}")]
+[String]$IPAddress,
+[Parameter(Mandatory,ParameterSetName="All")]
+[Switch]$All,
+[Switch]$Raw
+)
+
+Begin {
+    Write-Verbose "[BEGIN  ] Starting: $($MyInvocation.MyCommand)"  
+    #display PSBoundParameters formatted nicely for Verbose output  
+   [String]$pb = ($PSBoundParameters | Format-Table -AutoSize | Out-String).TrimEnd()
+   Write-Verbose "[BEGIN  ] PSBoundParameters: `n$($pb.split("`n").Foreach({"$("`t"*4)$_"}) | Out-String) `n" 
+
+    if (-not (Test-Path $lmfile )) {
+       Write-Warning "No file found at $lmfile. Use Set-LmhostsEntry to add an entry."
+       $Verified = $False
+    }
+    else {
+        $Verified = $True
+    }
+} #begin
+
+Process {
+
+    If ($Verified) {
+        Write-Verbose "[PROCESS] Get entry by $($PSCmdlet.ParameterSetName)"
+        Switch ($PSCmdlet.ParameterSetName) {
+
+        "Name" {
+            $find = $Computername
+          }
+        "IP" {
+            $find = $IPAddress
+         }
+         Default {
+            #get all entries
+         }
+        } #switch
+
+        if ($all) {
+            $data = Get-Content $lmfile | Select-String -pattern "^[^#.*]"
+            if ($raw) {
+                $IPv4Pattern.matches($data).value
+            }
+            else {
+              #create an object for each entry
+              $IPv4Pattern.matches($data).Captures | 
+              ForEach-Object -process {
+                [PSCustomObject]@{
+                    Computername = $_.Groups["Computername"]
+                    IPAddress = $_.Groups["IP"]
+                }
+              } #foreach
+            }
+        } #get all 
+        else {
+            Write-Verbose "[PROCESS] ...$find"
+            $entry = 
+            Get-Content -Path $lmfile | Select-String -pattern $find -AllMatches
+            if ($entry) {
+                Write-Verbose "[PROCESS] Found one or more matching entries"
+                if ($Raw) {
+                    Write-Verbose "[PROCESS] Writing raw results to the pipeline"
+                    #write the raw entry to the pipeline
+                    $entry
+                }
+                else {
+                    foreach ($item in $entry) {
+                      #split into parts and write a new object to the pipeline
+                      $m = $IPv4Pattern.Match($item)
+                     [PSCustomObject]@{
+                        Computername = $m.Groups["Computername"].value
+                        IPAddress = $m.Groups["IP"].value
+                     }
+                    } #foreach
+               } #else
+            } #if entry found
+            else {
+            Write-Warning "Entry for $($PSBoundParameters.values) not found."
+           }    
+     } #else find single entry
+     } #if verified
+}# process
+
+End {
+    Write-Verbose "[END    ] Ending: $($MyInvocation.MyCommand)"
+} #end
+}
---
+++ functions/Set-LmhostsEntry.ps1
@@ -0,0 +1,79 @@
+Function Set-LmhostsEntry {
+
+[CmdletBinding(SupportsShouldProcess)]
+Param(
+[Parameter(Mandatory,ValueFromPipelineByPropertyName)]
+[Alias("CN","Name")]
+[String]$Computername,
+[Parameter(Mandatory,ValueFromPipelineByPropertyName)]
+[ValidatePattern("(\d{1,3}\.){3}\d{1,3}")]
+[String]$IPAddress,
+[Switch]$PassThru
+)
+
+Begin {
+    Write-Verbose "[BEGIN  ] Starting: $($MyInvocation.MyCommand)"     
+    if (-not (Test-Path -path $lmfile )) {
+      Write-Verbose "[BEGIN  ] Creating a new lmhosts file: $lmfile"
+      Set-Content -Value "#lmhosts file for resolving NETBIOS names `n" -Path $lmfile -Encoding Ascii
+    }
+
+} #begin
+
+Process {
+
+    #display PSBoundParameters formatted nicely for Verbose output  
+    [String]$pb = ($PSBoundParameters | Format-Table -AutoSize | Out-String).TrimEnd()
+    Write-Verbose "[PROCESS] PSBoundParameters: `n$($pb.split("`n").Foreach({"$("`t"*2)$_"}) | Out-String) `n" 
+    
+    $entry = "$IPAddress  $Computername"
+            
+    Write-Verbose "[PROCESS] Check for existing entry $entry"
+    #There should only be one or at least we'll only process one
+    $existing = Get-Content -Path $lmfile | Select-String $Computername | Select-Object -last 1
+
+    if ($existing) {
+        [String]$line = $existing.Line
+        Write-Verbose "[PROCESS] Processing existing entry: $line"
+        
+        $m = $IPv4Pattern.Match($line)
+        $OldComputername = $m.Groups["Computername"].value
+        $oldIP = $m.Groups["IP"].value
+         Write-Verbose "Existing Computername = $oldComputername"
+         Write-Verbose "Existing IPAddress = $oldIP"   
+        if (($oldIP -eq $IPAddress) -and ($OldComputername -eq $Computername)) {
+            Write-Warning "The Computername and IP address matches the current entry. No changes needed."
+            #exit
+            Return
+        }
+        else {
+            #modify the line           
+            $Replace = $True
+        }
+    } #if existing
+
+    #create a backup copy of the file
+    Write-Verbose "[PROCESS] Creating a backup file copy"
+    Copy-Item -Path $lmfile -Destination $lmbackup
+
+    #insert the line
+    if ($Replace) {
+        $content = Get-Content -Path $lmfile
+        Write-Verbose "[PROCESS] Replacing $oldIP with $Ipaddress"
+        ($content -replace $line,$entry) | Out-File -FilePath $lmfile -Encoding ascii
+    }
+    else {
+        #insert at the end
+        Write-Verbose "[PROCESS] Appending $entry"
+        $entry | Out-File -FilePath $lmfile -Append -Encoding ascii        
+    }
+
+    If ($PassThru) {
+        Get-LmhostsEntry -Computername $Computername
+     }
+} #process
+
+End {
+    Write-Verbose "[END    ] Ending: $($MyInvocation.MyCommand)"
+} #end
+}
